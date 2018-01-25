ee9d7ea1fc97ad485afbc1da62a4e93d124a0e44
hbsis-rafaelwolf <rafael.wolf@hbsis.com.br>
2018-01-25 17:56:37 -0200

    first commit

---
+++ exercicios_webpack/ex/index.js
@@ -0,0 +1,34 @@
+/* EXEMPLO IMPORT  */
+
+// import Pessoa from "./pessoa";
+
+// const pessoa = new Pessoa('Rafael Wolf')
+// console.log(pessoa.toString())
+
+
+/* EXEMPLO SPREAD OPERATOR */
+
+// const produto = {
+//      nome : 'Caneta Bic Preta',
+//      preco: 1.90,
+//      desconto: 0.05
+// }
+
+// function clone(objeto){
+//     return { ...objeto }
+// }
+
+// const novoProduto = clone(produto)
+// novoProduto.nome = 'Caneta Bic Azul'
+
+// console.log(produto, novoProduto)
+
+import react from 'react'
+import './estilo.css'
+
+export default props => (
+    <h1>Olá</h1>
+)
+
+console.log('ok!!!')
+
