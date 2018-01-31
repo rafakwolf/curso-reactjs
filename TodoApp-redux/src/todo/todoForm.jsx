a4f3321b79d026546e2eb5c1da5afc90b5c163fa
Rafael Kaciano Wolf <rafakwolf@outlook.com>
2018-01-31 19:13:05 -0200

    add favicon

--- TodoApp-redux/src/todo/todoForm.jsx
+++ TodoApp-redux/src/todo/todoForm.jsx
@@ -28,32 +28,31 @@ class TodoForm extends Component {
     }
 
    render(){
-    const { add, search, description } = this.props
-    return (<div role="form" className="todoForm">
-                <Grid cols={'12 9 10'}>
-                    <input
-                        type="text"
-                        id="description"
-                        className="form-control"
-                        placeholder="Adicione uma tarefa"
-                        value={this.props.description}
-                        onChange={this.props.changeDescription}
-                        onKeyUp={this.keyHandler} />
-                </Grid>
-                <Grid cols={"12 3 2"}>
-                    <IconButton style="primary" icon="plus"
-                        onClick={()=> add(description)} />
+        const { add, search, description } = this.props
+        return (<div role="form" className="todoForm">
+                    <Grid cols={'12 9 10'}>
+                        <input
+                            type="text"
+                            id="description"
+                            className="form-control"
+                            placeholder="Adicione uma tarefa"
+                            value={this.props.description}
+                            onChange={this.props.changeDescription}
+                            onKeyUp={this.keyHandler} />
+                    </Grid>
+                    <Grid cols={"12 3 2"}>
+                        <IconButton style="primary" icon="plus"
+                            onClick={()=> add(description)} />
 
-                    <IconButton style="info" icon="search"
-                        onClick={() => search()} />
+                        <IconButton style="info" icon="search"
+                            onClick={() => search()} />
 
-                    <IconButton style="default" icon="close"
-                        onClick={this.props.handleClear} />
-                </Grid>
-            </div>
-        )
-    }
-
+                        <IconButton style="default" icon="close"
+                            onClick={this.props.handleClear} />
+                    </Grid>
+                </div>
+            )
+        }
 }
 
 const mapStateToProps = state => ({description: state.todo.description})
