482fabc6cc7e212c0deac3740702b937605f5d55
hbsis-rafaelwolf <rafael.wolf@hbsis.com.br>
2018-02-02 10:30:09 -0200

    my-money server finalizado

--- my-money-app/backend/src/config/routes.js
+++ my-money-app/backend/src/config/routes.js
@@ -1,12 +1,11 @@
 const express = require('express')
 
 module.exports = function(server){
-    
-    // base
+    // Base
     const router = express.Router()
     server.use('/api', router)
 
-    // ciclos de pagamentos
+    // Ciclos de pagamentos
     const BillingCycle = require('../api/billingCycle/billingCycleService')
     BillingCycle.register(router, '/billingCycles')
 }
--- my-money-app/backend/src/config/server.js
+++ my-money-app/backend/src/config/server.js
@@ -2,10 +2,14 @@ const port = 3003
 
 const express = require('express')
 const bodyParser = require('body-parser')
+const allowCors = require('./cors')
+const queryParser = require('express-query-int')
 const server = express()
 
 server.use(bodyParser.urlencoded({  extended: true }))
 server.use(bodyParser.json())
+server.use(allowCors)
+server.use(queryParser())
 
 server.listen(port, function(){
     console.log(`Server running on port ${port}.`)
