5fbaa32a32c345a7e1c6b1d7baace1882dc66477
hbsis-rafaelwolf <rafael.wolf@hbsis.com.br>
2018-02-02 14:36:49 -0200

    my-money-app frontend

---
+++ my-money-app/frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+    <footer className="main-footer">
+        <strong>
+            Copyright &copy; 2018
+            <a href="http://rafakwolf.blogspot.com.br" target="_blank">&nbsp;rafakwolf</a>
+        </strong>
+    </footer>
+)
