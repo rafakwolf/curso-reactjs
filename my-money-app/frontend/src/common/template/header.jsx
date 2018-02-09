941ad0ebaf0fa5d373165d5e515cb7bf423c77c9
Rafael Kaciano Wolf <rafakwolf@outlook.com>
2018-02-08 23:08:30 -0200

    front-end final

--- my-money-app/frontend/src/common/template/header.jsx
+++ my-money-app/frontend/src/common/template/header.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Navbar from './navbar'
 
 export default props => (
     <header className="main-header">
@@ -11,10 +12,11 @@ export default props => (
                 <i className="fa fa-money"></i>
                 <b>&nbsp;My</b>&nbsp;Money
             </span>
-        </a>
+        </a>        
 
         <nav className="navbar navbar-static-top">
             <a href='#' className="sidebar-toggle" data-toggle="offcanvas"></a>
+            <Navbar />
         </nav>
     </header>
 )
